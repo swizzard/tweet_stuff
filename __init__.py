c52a85567fda8c654a11d5381dad9a5e1f70e613
Sam Raker <sam.raker@gmail.com>
2013-11-30 14:27:01 -0500

    Cleaned out old/out-of-date/unnecessary stuff, recreated wordlists with proper documentation.
    Details:
    Deleted all the django/database-related stuff.
    Deleted all previously-existing wordlists.
    Deleted json_tools.py and longitudinal.py as being out-of-date and unnecessary.
    Modified __init__ to remove references to django/database-related stuff.
    Added corpora_used.txt, which is a reference list of all the NLTK corpora used to construct the wordlists.
    Added words.txt, which is a list of all the words extracted from the corpora cited in corpora_used.txt.
    Added words_lowered.txt, which is the same words as in words.txt, but lowercased using .lower().
    Added freqs.json, which is a json dump of a dictionary of words from words.txt and their frequencies in the list.
    Added lowered_freqs.json, which is a json dump of a dictionary of words from lowered_words.txt and their frequencies in the list.

--- __init__.py
+++ __init__.py
@@ -1,24 +1,6 @@
 __author__ = 'samuelraker'
 
-import sys,os
-# sys.path.insert(1,os.path.abspath('tweet_stuff'))
-# sys.path.insert(1,os.path.abspath('../samrakerdotcom'))
-# sys.path.insert(1,os.path.abspath('../hash_to_hash'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'samrakerdotcom.settings'
-#from django.conf import settings
-#settings.configure()
-import re
-import json
-import twitter
-try:
-    from hash_to_hash.models import Tweet, Hashtag, Competitors
-    from django.db import IntegrityError
-    from django.db import DatabaseError
-except ImportError as e:
-    errs = e.args
-    for err in errs:
-        print err
-    print "Database-related tools will be unavailable..."
+
 from tools import auth
 from parsed_tweet import ParsedTweet
 
